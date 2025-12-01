f3d4c7e5392af39ea1da34bac2a04978a8d2f928
POOMAGAL <ctpoomagall@gmail.com>
2025-12-01 11:24:26 +0300

    training added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -4,28 +4,59 @@ from zipfile import ZipFile
 import tensorflow as tf
 import time
 from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
+from pathlib import *
+
+
+# class PrepareCallback:
+#     def __init__(self, config: PrepareCallbacksConfig):
+#         self.config = config
+
+#     @property
+#     def _create_tb_callbacks(self):
+#         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+#         tb_running_log_dir = os.path.join(
+#             self.config.tensorboard_root_log_dir,
+#             f"tb_logs_at_{timestamp}",
+#         )
+#         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+
+
+#     @property
+#     def _create_ckpt_callbacks(self):
+#         return tf.keras.callbacks.ModelCheckpoint(
+#             filepath=self.config.checkpoint_model_filepath,
+#             save_best_only=True
+#         )
+
+
+#     def get_tb_ckpt_callbacks(self):
+#         return [
+#             self._create_tb_callbacks,
+#             self._create_ckpt_callbacks
+#         ]
+import os
+import time
+import tensorflow as tf
 
 class PrepareCallback:
-    def __init__(self, config: PrepareCallbacksConfig):
+    def __init__(self, config):
         self.config = config
 
     @property
     def _create_tb_callbacks(self):
         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
         tb_running_log_dir = os.path.join(
-            self.config.tensorboard_root_log_dir,
+            str(self.config.tensorboard_root_log_dir),
             f"tb_logs_at_{timestamp}",
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
 
-
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
+            filepath=str(self.config.checkpoint_model_filepath),  # FIXED
             save_best_only=True
         )
-
 
     def get_tb_ckpt_callbacks(self):
         return [
